fa8f0b81d0a71866311bdad77b6bd8a52b8e0b70
jlbarraganm <160335263+jlbarraganm@users.noreply.github.com>
2024-02-24 21:59:21 +0000

    probable audio 1

--- src/App.js
+++ src/App.js
@@ -5,13 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          imagen primero 1
-        </p>
+        <p>imagen primero 1</p>
         <img src='https://img.freepik.com/vector-gratis/atomo-ilustracion-modelo-electrones-neutrones-aislados_1284-53084.jpg?size=338&ext=jpg&ga=GA1.1.1546980028.1708732800&semt=ais' />
         <p>Ejemplo de audio</p>
-        <source src="https://artlist.io/royalty-free-music/song/haymaker/122647" type="audio/mpeg" />
+        <source src="hhttps://samplelib.com/lib/preview/mp3/sample-15s.mp3" type="audio/mpeg" />
       </header>
     </div>
   );
