de09c02e468d54dde09f2d0887754f29738ecbcc
jlbarraganm <160335263+jlbarraganm@users.noreply.github.com>
2024-02-24 22:29:55 +0000

    Audio-video-imagen

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ function App() {
           <source src="https://samplelib.com/lib/preview/mp3/sample-15s.mp3" type="audio/mpeg"/>
           "Tu navegador no soporta el elemento de audio."
         </audio>
+        <p>Video ejemplo 3</p>
       <video controls width="600">
         <source src="https://samplelib.com/lib/preview/mp4/sample-30s.mp4" type="video/mp4" />
         Tu navegador no soporta el elemento de video.
