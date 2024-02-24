786e7ef761c44be1be1a97d153aef31bd476a9fe
jlbarraganm <160335263+jlbarraganm@users.noreply.github.com>
2024-02-24 21:47:28 +0000

    audio por favor 2

--- src/App.js
+++ src/App.js
@@ -11,7 +11,6 @@ function App() {
         </p>
         <img src='https://img.freepik.com/vector-gratis/atomo-ilustracion-modelo-electrones-neutrones-aislados_1284-53084.jpg?size=338&ext=jpg&ga=GA1.1.1546980028.1708732800&semt=ais' />
         <p>Ejemplo de audio</p>
-      <audio controls>
         <source src="https://artlist.io/royalty-free-music/song/haymaker/122647" type="audio/mpeg" />
       </header>
     </div>
