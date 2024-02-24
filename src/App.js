5283f0ec60d7a9feef41e5f2b1a3b6fb8a764eba
jlbarraganm <160335263+jlbarraganm@users.noreply.github.com>
2024-02-24 20:01:22 +0000

    con imagen cambio

--- src/App.js
+++ src/App.js
@@ -7,10 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-          Acá estoy editando App.js
+          imagen primero
         </p>
-        <p>Imagen de un átomo</p>
         <img src='https://img.freepik.com/vector-gratis/atomo-ilustracion-modelo-electrones-neutrones-aislados_1284-53084.jpg?size=338&ext=jpg&ga=GA1.1.1546980028.1708732800&semt=ais' />
         <a
           className="App-link"
