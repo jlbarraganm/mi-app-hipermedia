9cf5b0e3bd4a4db7fb886465bf16f08e1e400bb0
jlbarraganm <160335263+jlbarraganm@users.noreply.github.com>
2024-02-24 21:42:41 +0000

    ultimo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          imagen primero
+          imagen primero 1
         </p>
         <img src='https://img.freepik.com/vector-gratis/atomo-ilustracion-modelo-electrones-neutrones-aislados_1284-53084.jpg?size=338&ext=jpg&ga=GA1.1.1546980028.1708732800&semt=ais' />
       </header>
