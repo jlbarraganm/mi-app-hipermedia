b3dd1b5dc1dd092ed0784b750783a5991ae159e7
jlbarraganm <160335263+jlbarraganm@users.noreply.github.com>
2024-02-24 21:35:11 +0000

    App con audio

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           imagen primero
         </p>
         <img src='https://img.freepik.com/vector-gratis/atomo-ilustracion-modelo-electrones-neutrones-aislados_1284-53084.jpg?size=338&ext=jpg&ga=GA1.1.1546980028.1708732800&semt=ais' />
-        <h1>Ejemplo de audio en JSX</h1>
+        <h1>Audio</h1>
         <source src="https://artlist.io/royalty-free-music/song/groove-parade/127651"/>
       </header>
     </div>
